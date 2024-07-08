cea42cfbbcccb88ade421ceb193f52e7603e18d2
Open Kitchens <123849081+OpenKitchens@users.noreply.github.com>
2024-07-08 22:02:03 +0900

    gg

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-import vercel from "@astrojs/vercel/serverless";
-
-// https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
+  outDir: 'dist',
+  // 追加の設定が必要な場合はここに記述
 });
